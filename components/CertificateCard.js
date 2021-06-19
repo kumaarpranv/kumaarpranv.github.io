366bc406ddc1ca0acd11bac42af1098f377a2a61
Pranav Kumar <kumaarpranv@gmail.com>
2021-06-19 14:48:45 +0530

    fixed scaling of images

--- components/CertificateCard.js
+++ components/CertificateCard.js
@@ -12,10 +12,10 @@ export default function CertificateCard({ title, href, image }) {
       <div className="flex items-center border border-gray-200 dark:border-gray-800 rounded p-4">
         <img className="h-20 w-8 min-w-lg text-gray-900 dark:text-gray-100" 
         style={{
-          width: "1200px",
-          height: "1000px",
-          maxHeight: "1200px",
-          maxWidth: "1000px",
+          width: "600px",
+          height: "500px",
+          maxHeight: "600px",
+          maxWidth: "500px",
         }}
         src={"/certificates/"+image} alt="me" />
     </div>
